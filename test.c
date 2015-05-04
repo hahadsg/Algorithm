847c6dbdca543fd747aad4ab693124a37785d71d
詹杰凡 <jiefan.zhan@gumichina.com>
2015-05-04 18:36:17 +0800

    kd_tree完成

--- test.c
+++ test.c
@@ -7,9 +7,6 @@
 #include "src/kd_tree.h"
 #include "src/heap.h"
 
-/* kd_tree */
-void test_kd_tree(int argc, char **argv);
-void test_kd_tree_print(kd_node* kd_root, int level, char c);
 /* cmp */
 int cmp_int(const void *a, const void *b); 
 
@@ -18,7 +15,7 @@ int cmp_int(const void *a, const void *b);
 /********************************************************************************/
 int main(int argc, char **argv)
 {
-    test_kd_tree(argc, argv);
+    // test(argc, argv);
     return 0;
 }
 
@@ -28,62 +25,6 @@ int main(int argc, char **argv)
 int cmp_int(const void *a, const void *b) 
 {
     return *(int*)a - *(int*)b;
-}
-
-/********************************************************************************/
-/* kd_tree */
-/********************************************************************************/
-void test_kd_tree(int argc, char **argv) 
-{
-    int n = 6;
-    double a[6][2] = {{2,3}, {5,4}, {9,6}, {4,7}, {8,1}, {7,2}};
-    point *points, p, res;
-    double min_dist;
-    kd_node *kd_root;
-    int i, j;
-    int d = 2;
-
-    points = (point*)malloc(n*sizeof(point));
-    for (i = 0; i < n; i++) {
-        points[i].v[0] = a[i][0];
-        points[i].v[1] = a[i][1];
-    }
-
-    // build
-    kd_root = kd_tree_build(points, n, 2);
-    printf("----- kd_tree(a) -----\n");
-    test_kd_tree_print(kd_root, 1, 'h');
-
-    // insert
-    // p.v[0] = 5;
-    // p.v[1] = 3;
-    // kd_tree_insert(kd_root, &p, 2);
-    // printf("----- kd_tree(a) -----\n");
-    // test_kd_tree_print(kd_root, 1, 'h');
-
-    // delete
-    // kd_root = kd_tree_delete(kd_root, 2);
-    // printf("----- kd_tree(a) -----\n");
-    // test_kd_tree_print(kd_root, 1, 'h');
-
-    // find closest
-    p.v[0] = 6;
-    p.v[1] = 5;
-    min_dist = -1;
-    kd_tree_find_closest(kd_root, &p, &res, &min_dist);
-    printf("point(%lf, %lf) -> point(%lf, %lf), min_dist=%lf\n", p.v[0], p.v[1], res.v[0], res.v[1], min_dist);
-
-    // free
-    kd_tree_free(kd_root);
-}
-
-void test_kd_tree_print(kd_node* kd_root, int level, char c) 
-{
-    printf("lv:%d%c, point:(%lf, %lf), split=%d\n", level, c, kd_root->point.v[0], kd_root->point.v[1], kd_root->split);
-    if (kd_root->left)
-        test_kd_tree_print(kd_root->left, level+1, 'l');
-    if (kd_root->right)
-        test_kd_tree_print(kd_root->right, level+1, 'r');
 }
 
 
--- test_hist.c
+++ test_hist.c
@@ -132,7 +132,8 @@ void test_kd_tree(int argc, char **argv)
 {
     int n = 6;
     double a[6][2] = {{2,3}, {5,4}, {9,6}, {4,7}, {8,1}, {7,2}};
-    point *points, p;
+    point *points, p, res;
+    double min_dist;
     kd_node *kd_root;
     int i, j;
     int d = 2;
@@ -156,9 +157,16 @@ void test_kd_tree(int argc, char **argv)
     // test_kd_tree_print(kd_root, 1, 'h');
 
     // delete
-    kd_root = kd_tree_delete(kd_root, 2);
-    printf("----- kd_tree(a) -----\n");
-    test_kd_tree_print(kd_root, 1, 'h');
+    // kd_root = kd_tree_delete(kd_root, 2);
+    // printf("----- kd_tree(a) -----\n");
+    // test_kd_tree_print(kd_root, 1, 'h');
+
+    // find closest
+    p.v[0] = 6;
+    p.v[1] = 5;
+    min_dist = -1;
+    kd_tree_find_closest(kd_root, &p, &res, &min_dist);
+    printf("point(%lf, %lf) -> point(%lf, %lf), min_dist=%lf\n", p.v[0], p.v[1], res.v[0], res.v[1], min_dist);
 
     // free
     kd_tree_free(kd_root);
@@ -172,7 +180,6 @@ void test_kd_tree_print(kd_node* kd_root, int level, char c)
     if (kd_root->right)
         test_kd_tree_print(kd_root->right, level+1, 'r');
 }
-
 
 /********************************************************************************/
 /* randomized_select */
